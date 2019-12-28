96746e617107e9307b3e0dc8e708008df6913d13
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2019-12-29 07:52:36 +0900

    add message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,13 @@
 class MessagesController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  private
+
+  def create_message
+    params.require(:message).permit(:body, :image)
+  end
 end
