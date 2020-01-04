2fd9c4d92d52963a7c3a48e3d8228b0f2d39d7c5
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-01-04 16:23:04 +0900

    add pry-rails

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Message, type: :model do
   describe '#create' do
     context 'can save' do
-      it 'is valid with body' do
+      it 'is valid with content' do
         expect(build(:message, image: nil)).to be_valid
       end
 
@@ -11,28 +11,28 @@ RSpec.describe Message, type: :model do
         expect(build(:message, body: nil)).to be_valid
       end
 
-      it 'is valid with body and image' do
+      it 'is valid with content and image' do
         expect(build(:message)).to be_valid
       end
     end
 
     context 'can not save' do
-      it 'is invalid without body and image' do
+      it 'is invalid without content and image' do
         message = build(:message, body: nil, image: nil)
         message.valid?
-        expect(message.errors.added?(:body, :blank)).to be_truthy
+        expect(message.errors[:body]).to include("を入力してください")
       end
 
       it 'is invalid without group_id' do
         message = build(:message, group_id: nil)
         message.valid?
-        expect(message.errors[:group]).to include("must exist")
+        expect(message.errors[:group]).to include("を入力してください")
       end
 
       it 'is invaid without user_id' do
         message = build(:message, user_id: nil)
         message.valid?
-        expect(message.errors[:user]).to include("must exist")
+        expect(message.errors[:user]).to include("を入力してください")
       end
     end
   end
