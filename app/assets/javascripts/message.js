ba3c042d89f51d37a7ef5d01b2eb927cde0ae7ca
Daiki-Abe <58421780+Daiki-Abe@users.noreply.github.com>
2020-01-06 21:37:14 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,32 +2,34 @@ $(function(){
     function buildHTML(message){
     if (message.image) {
       var html =
-        `<div class="user-message__name">
-          <div class="user-message__name__talker">
-            ${message.user_name}
+        `<div class="message" data-message-id= ${message.id} >
+          <div class="user-message__name">
+            <div class="user-message__name__talker">
+              ${message.user_name}
+            </div>
+            <div class="user-message__name__day-time">
+              ${message.created_at}
+            </div>
           </div>
-          <div class="user-message__name__day-time">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="user-message__message">
-          ${message.body}
-          <img class="lower-message__image" src= ${message.image} >
-        </div>`
+          <div class="user-message__message">
+            ${message.body}
+            <img class="lower-message__image" src= ${message.image} >
+          </div>`
       return html;
     } else {
       var html = 
-        `<div class="user-message__name">
-        <div class="user-message__name__talker">
-          ${message.user_name}
-        </div>
-        <div class="user-message__name__day-time">
-          ${message.created_at}
-        </div>
-      </div>
-      <div class="user-message__message">
-        ${message.body}
-      </div>`
+        `<div class="message" data-message-id= ${message.id} >
+          <div class="user-message__name">
+            <div class="user-message__name__talker">
+              ${message.user_name}
+            </div>
+            <div class="user-message__name__day-time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="user-message__message">
+            ${message.body}
+          </div>`
       return html;
     };
   }
@@ -54,4 +56,25 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
   });
 })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
 });
